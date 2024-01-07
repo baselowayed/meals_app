788ff5d07c6de39b0a51d4b51ea1fbaafdaea1f9
Basel <baselowayed98@gmail.com>
2024-01-07 19:56:02 +0200

    adding the code after moving to disk c

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
+
+  Widget build(context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor, //visual taping effect color
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
+  }
+}
