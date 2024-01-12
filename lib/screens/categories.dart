2f011b9b53d5972f31e3d181f806d8a24f793b77
baselowayed <baselowayed98@gmail.com>
2024-01-12 23:33:43 +0200

    adding an explicit animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,15 +5,46 @@ import 'package:meals_app/widgets/category_grid_item.dart';
 import 'package:meals_app/models/category.dart';
 import 'package:meals_app/models/meal.dart';
 
-
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
 
   // final void Function(Meal meal) onToggleFavorite;
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  // "with" allows us to add a socalled Mixin to a class, as another class merged with this class behinds the scenes and offer certain features
+  late AnimationController
+      _animationController; //will have a value when its used for the first time but currently it is not a variable
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController
+        .forward(); //this will start the animation / use repeat to play the animation in a loop
+  }
+
+  @override
+  void dispose() {
+    _animationController
+        .dispose(); //this makes sure that this animationcontroller is removed from the device memory once this widget here is removed(avoiding memory overflows)
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -30,23 +61,43 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-          //complex way of saying I can set the number of the columns I wanna have here default = 2
-          padding: const EdgeInsets.all(24),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          children: [
-            //availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-            for (final category in availableCategories)
-              CategoryGridItem(
-                category: category,
-                onSelectCategory: () {
-                  _selectCategory(context , category);
-                },
-              )
-          ]);
+    return AnimatedBuilder(
+        animation: _animationController,
+        child: GridView(
+            //complex way of saying I can set the number of the columns I wanna have here default = 2
+            padding: const EdgeInsets.all(24),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20),
+            children: [
+              //availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+              for (final category in availableCategories)
+                CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                )
+            ]),
+        builder: (contex, child) => SlideTransition(
+              //# 2 way of explicit animation
+              position: Tween(
+                begin: const Offset(0, 0.3), //30% offset
+                end: const Offset(0, 0),
+              ).animate(
+                CurvedAnimation(
+                  parent: _animationController,
+                  curve: Curves.easeInOut,
+                ),
+              ),
+              child: child,
+            )
+        //Padding(//#1 way of explicit animation
+        //   padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+        //   child: child,
+        // ),
+        );
   }
 }
